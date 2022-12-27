3a8ab6291ce8f4d135520f838958704db0678d5e
tesfalem tekie <teshanna72016@gmail.com>
2022-12-27 17:39:56 -0600

    first commit

---
+++ day-one/day-one.js
@@ -0,0 +1,29 @@
+$(function() {
+   
+    // this function listens to keydown event
+    function playSounds(e) {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if(!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    }
+
+    // this function listens to transitionend event
+    function removeTransition(e) {
+        if(e.propertyName !== 'transform') {
+            return;
+        }
+        this.classList.remove('playing');
+    }
+
+    let keys = document.querySelectorAll('.key');
+
+    keys.forEach(key => {
+       key.addEventListener('transitionend', removeTransition);
+    });
+
+    window.addEventListener('keydown', playSounds);
+    
+});
