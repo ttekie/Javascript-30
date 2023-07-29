eb0485fca9ce3b927ede554e6673615051aa9f12
tesfalem tekie <teshanna72016@gmail.com>
2023-07-29 14:35:48 -0500

    day eight practice completed

--- 01 - Javascript Drum Kit/day-one.js
+++ 01 - Javascript Drum Kit/day-one.js
@@ -4,7 +4,7 @@ $(function() {
     function playSounds(e) {
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-        if(!audio) return;
+        if(!audio) return; // this will stop the function from running if there is no audio
         audio.currentTime = 0;
         audio.play();
         key.classList.add('playing');
